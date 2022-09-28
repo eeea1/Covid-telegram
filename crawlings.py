bd606b1c122a473ed9710475816edcd7143813e0
eeea1 <103316263+eeea1@users.noreply.github.com>
2022-09-28 21:41:47 +0900

    Update crawlings.py

--- crawlings.py
+++ crawlings.py
@@ -9,7 +9,8 @@ url = 'http://apis.data.go.kr/1352000/ODMS_COVID_02/callCovid02Api'
 params ={'serviceKey' : Servicekey, 'pageNo' : '1', 'numOfRows' : '500', 'apiType' : 'xml', 'status_dt' : '20220906' }
 response =requests.get(url, params=params) #url
 html = response.text #url을 해석하기 쉽게 텍스트형태로 전환.
-soup = BeautifulSoup(html, 'html.parser') #bs4를 토토sp = soup.find('item').get_text()
+soup = BeautifulSoup(html, 'html.parser')
+sp = soup.find('item').get_text()
 b = (sp.split('\n'))
 j = 0
 for i in range(1,12):
