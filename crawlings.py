cdbd6484fa58487caa9a16efc55eaa1d3d5cff63
eeea1 <103316263+eeea1@users.noreply.github.com>
2022-10-12 21:27:29 +0900

    Update crawlings.py

--- crawlings.py
+++ crawlings.py
@@ -1,27 +1,21 @@
-from bs4 import BeautifulSoup
-import requests
 import json
 import os
-from datetime import timedelta, datetime
-ydt = datetime.now() - timedelta(days=1)
-yd = ydt.strftime('%Y%m%d')
-Servicekey = os.environ['SERVICEKEY']
-avl = {}
-url = 'http://apis.data.go.kr/1352000/ODMS_COVID_02/callCovid02Api'
-params ={'serviceKey' : Servicekey, 'pageNo' : '1', 'numOfRows' : '500', 'apiType' : 'xml', 'status_dt' : yd }
-response =requests.get(url, params=params) #url
-html = response.text #url을 해석하기 쉽게 텍스트형태로 전환.
-soup = BeautifulSoup(html, 'html.parser')
-sp = soup.find('item').get_text()
-b = (sp.split('\n'))
-j = 0
-for i in range(1,12):
-    j += i # 1,3,5,7...
-    tt = soup.item.contents[j]
-    avl[tt.name] = b[i]
-    j = i
+import requests
+from bs4 import BeautifulSoup
+
+apikey = os.environ['SERVICEKEY'] # API 키를 다음 란에 입력해 주세요.
+korea = "https://api.corona-19.kr/korea/beta/?serviceKey="
+response = requests.get(korea + apikey)
+message = response.text
+
+
+
+# 크롤링 방법 1: BeautifulSoup 활용하기
+soup = BeautifulSoup(message, 'html.parser') # parser 종류는 xml, html 선택 가능
+sp = soup
+result = eval(sp.get_text())["korea"]
 with open('./result.json', 'w') as file:
-    json.dump(avl, file)
+    json.dump(result, file)
 with open('./result.json') as file:
     contents = file.read()
     load_data = json.loads(contents)
